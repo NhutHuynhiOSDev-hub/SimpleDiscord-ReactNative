14d9a37d90587438cc0556df3f03aa41e6e98df7
Nhut Huynh VeNtura <nhuthq@zigexn.vn>
2022-03-08 12:10:59 +0700

    update project structures

--- simple_discord_react_native/App.tsx
+++ simple_discord_react_native/App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './.expo/src/hooks/useCachedResources';
+import useColorScheme from './.expo/src/hooks/useColorScheme';
+import Navigation from './.expo/src/navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
